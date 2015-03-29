0950b9583388faa76fc71b3f5087418e1e552c8a
Nate Straub <nate.straub@goblue20.com>
2015-03-29 16:20:16 -0500

    Added routing and modified the data model to fit the existing BIL model.

---
+++ server/fixtures.js
@@ -0,0 +1,23 @@
+if(Issues.find().count() === 0) {
+	Issues.insert({
+		type: 'Issue',
+		username: 'nstraub',
+		submit_date: '03-01-2015',
+		task_id: '1',
+		process_name: 'Claims Appeals',
+		task_name: 'Review Claim Appeal',
+		description: 'A random description',
+		priority: 'High',
+		status: 'In Progress',
+		assigned_to: 'nstraub',
+		resolve_by: '03-31-2015',
+		analysis: '',
+		resolved_on: '',
+		follow_up_email: '',
+		resolution: '',
+		fixed_snapshot: '',
+		votes: '',
+		estimated_effort: '',
+		attachment: ''
+	});
+}
